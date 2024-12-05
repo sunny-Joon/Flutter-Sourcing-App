963466c315a1ffb23640e49b36a3b849ea0b8c0c
ragvendra <you@example.com>
2024-12-05 14:39:15 +0530

    12/05/2024 leaderboard change

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_sourcing_app/GlobalClass.dart';
-import 'package:flutter_sourcing_app/LoginPage.dart';
-import 'package:flutter_sourcing_app/qr_PaymentReports.dart';
-import 'package:flutter_sourcing_app/utils/currentLocation.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_sourcing_app/qr_payment_reports.dart';
+import 'package:flutter_sourcing_app/utils/current_location.dart';
 
-import 'ApiService.dart';
+import 'api_service.dart';
+import 'global_class.dart';
+import 'login_page.dart';
+
 
 class Profile extends StatefulWidget {
   @override
